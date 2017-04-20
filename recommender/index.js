0d9da670ab78fee9520591e90fee5b475e2a6832
Konstantin Azizov <azizovkostya97@gmail.com>
2017-04-20 18:34:22 +0300

    Move queue handing to external file

--- recommender/index.js
+++ recommender/index.js
@@ -1,21 +1,13 @@
 'use strict';
 
-const kue = require('kue');
 
 const log = require('./lib/logger');
 const { sequelize } = require('./models');
 const processUser = require('./lib/process-user');
 const CrawlerAPI = require('./lib/crawler-api');
+const queue = require('./lib/queue');
 
-let queue = kue.createQueue({
-  prefix: 'r',
-  redis: {
-    port: process.env.REDIS_PORT_6379_TCP_PORT,
-    host: 'redis'
-  }
-});
-
-queue.process('recommendation', function(job, done) {
+queue.process(function(job, done) {
   processUser(job.data.id).then(function() {
     done();
   }).catch(function(err) {
@@ -24,31 +16,10 @@ queue.process('recommendation', function(job, done) {
   });
 });
 
-queue.on('error', log.error);
-
-process.once('SIGTERM', function () {
-  queue.shutdown(60000, function(err = '') {
-    log.info('Queue shutdown: ', err);
-    process.exit(0);
-  });
-});
-
 sequelize.authenticate().then(function() {
   return sequelize.sync();
 }).then(function () {
-  if(process.env.DASHBOARD_PORT) {
-    log.info('Dashboard available at:', process.env.DASHBOARD_PORT)
-    kue.app.listen(process.env.DASHBOARD_PORT);
-    kue.app.set('title', 'Recommender');
-  }
-
-  return new Promise(function(resolve, reject) {
-    queue.inactiveCount(function(err, total) {
-      if(err) return reject(err);
-
-      resolve(total);
-    });
-  });
+  queue.getPending();
 }).then(function(currentQueue) {
   if(currentQueue) {
     return [];
@@ -56,12 +27,12 @@ sequelize.authenticate().then(function() {
 
   return CrawlerAPI.getUserList();
 }).then(function(users) {
-  users.forEach(function(user) {
-    let id = user.id;
+  let ids = users.map((user) => user.id);
 
-    queue.create('recommendation', {
+  ids.forEach(function(id) {
+    queue.create({
       title: 'Processing ' + id,
       id
-    }).priority('low').save();
+    });
   });
 }).catch(log.error);
---
+++ recommender/lib/queue.js
@@ -0,0 +1,47 @@
+const kue = require('kue');
+const log = require('./logger');
+
+const JOB_NAME = 'recommendation';
+
+let queue = kue.createQueue({
+  prefix: 'r',
+  redis: {
+    port: process.env.REDIS_PORT_6379_TCP_PORT,
+    host: 'redis'
+  }
+});
+
+queue.on('error', log.error);
+
+if(process.env.DASHBOARD_PORT) {
+  kue.app.listen(process.env.DASHBOARD_PORT);
+  kue.app.set('title', JOB_NAME);
+  log.info('Dashboard available at:', process.env.DASHBOARD_PORT);
+}
+
+process.once('SIGTERM', function () {
+  queue.shutdown(60000, function(err = '') {
+    log.info('Queue shutdown: ', err);
+    process.exit(0);
+  });
+});
+
+module.exports = {
+  process: function(handler) {
+    queue.process(JOB_NAME, handler);
+  },
+
+  getPending: function () {
+    return new Promise(function(resolve, reject) {
+      queue.inactiveCount(function(err, total) {
+        if(err) return reject(err);
+
+        resolve(total);
+      });
+    });
+  },
+
+  create: function(data) {
+    queue.create(JOB_NAME, data).priority('low').save();
+  }
+};
