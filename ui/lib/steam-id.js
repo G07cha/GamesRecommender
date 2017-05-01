b5c09eab85e85d60879a145379a7705c75255e3c
Konstantin Azizov <azizovkostya97@gmail.com>
2017-05-01 14:51:15 +0300

    Add check for regular steam ID

--- ui/lib/steam-id.js
+++ ui/lib/steam-id.js
@@ -1,20 +1,26 @@
 'use strict';
 
 const SteamAPI = require('./steam-api');
+const steamIDRegEx = /\d{17}/;
 
 let steamAPI = new SteamAPI();
 
 function steamIdResolver(req, res) {
   return new Promise(function(resolve, reject) {
     if(req.body.value) {
-      steamAPI.getUserId(req.body.value).then(function(steamId) {
-        if(!steamId) {
-          reject(`Cannot find steam id for ${req.body.value}`);
-        }
+      // Check if it's already a regular ID
+      if(steamIDRegEx.test(req.body.value)) {
+        resolve(req.body.value);
+      } else {
+        steamAPI.getUserId(req.body.value).then(function(steamId) {
+          if(!steamId) {
+            reject(`Cannot find steam id for ${req.body.value}`);
+          }
 
-        res.cookie('steamId', steamId);
-        resolve(steamId);
-      });
+          res.cookie('steamId', steamId);
+          resolve(steamId);
+        });
+      }
     } else if (req.cookies.steamId) {
       resolve(req.cookies.steamId);
     } else {
